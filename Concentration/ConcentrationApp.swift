9bf79f8119fdcc55d399cfea431e84e44cddefab
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-02-19 21:45:47 -0500

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Jorge Caraballo on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
